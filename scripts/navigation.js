25707b521bcc17cc025073c1bcf8e01e7a112b15
julianbrom1 <bec21001@byui.edu>
2025-03-11 19:43:00 -0500

    final

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -4,4 +4,10 @@ const navigation = document.querySelector('.navigation');
 hamButton.addEventListener('click', () => {
   navigation.classList.toggle('open');
   hamButton.classList.toggle('open');
+
+  if (hamButton.classList.contains('open')) {
+    hamButton.setAttribute('aria-label', 'Close navigation menu');
+  } else {
+    hamButton.setAttribute('aria-label', 'Open navigation menu');
+  }
 });
